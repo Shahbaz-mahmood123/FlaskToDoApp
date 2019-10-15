119e56ca0fd64d7e4bed2ce34e97212c5ee561cf
shahzeb mahmood <shahbaz1997@hotmail.co.uk>
2019-10-15 14:30:14 +0100

    fix update route and html and add delete functionality

--- flasktodo/routes.py
+++ flasktodo/routes.py
@@ -112,11 +112,11 @@ def update_list(list_id):
     if lists.user_id != current_user.get_id():
         abort(403)
     if form.validate_on_submit():
-            lists.title = form.title.data
-            lists.content = form.content.data
-            db.session.commit()
-            flash('Your list has been updated')
-            return redirect(url_for('list', list_id = list_id))
+        lists.title = form.title.data
+        lists.content = form.content.data
+        db.session.commit()
+        flash('Your list has been updated')
+        return redirect(url_for('list', list_id = list_id))
         ##issue with this route  not working properly, redirectig to list page without showing the update form
     elif request.method == 'GET':
         form.title.data = lists.title
@@ -124,7 +124,11 @@ def update_list(list_id):
     return render_template('create_list.html', title ='Update List', form = form, legend= 'Update Post' )
 
 
-@app.route("/list/<int:list_id>/delete", methods=['GET', 'POST'])
+@app.route("/list/<int:list_id>/delete", methods=['POST'])
 @login_required
 def delete_list(list_id):
-    return render_template('delete_list.html')
+    list = Lists.query.get_or_404(list_id)
+    db.session.delete(list)
+    db.session.commit()
+    flash('List has been deleted')
+    return redirect(url_for('home'))
